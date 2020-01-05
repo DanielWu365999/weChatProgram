29146a43a6bd4631cb0c2649e605846d694a3a93
WuPengFei123 <498132853@qq.com>
2020-01-05 19:20:25 +0800

    dafeigegeg

--- pages/myAnswer/login.js
+++ pages/myAnswer/login.js
@@ -5,14 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 查看是否授权
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
@@ -65,5 +77,8 @@ Page({
   },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  bindGetUserInfo(e) {
+    console.log(e.detail.userInfo)
   }
 })
